356ebde7fe2c942ae75a7a8d24c00726c4ed1ab3
Aaron Cuadrado <163054420+AaronCuadrado@users.noreply.github.com>
2024-10-09 17:45:34 +0000

    alembic version modificado

--- src/models.py
+++ src/models.py
@@ -3,19 +3,16 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 class User(db.Model):
+    __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
-
-    def __repr__(self):
-        return '<User %r>' % self.username
+    username = db.Column(db.String(50), nullable=False)
+    favorite_people = db.relationship('Favorite_People', backref='user', lazy=True)
+    favorite_planets = db.relationship('Favorite_Planet', backref='user', lazy=True)
 
     def serialize(self):
         return {
-            "id": self.id,
-            "email": self.email,
-            # do not serialize the password, its a security breach
+        "id": self.id, 
+        "username": self.username, 
         }
     
 
